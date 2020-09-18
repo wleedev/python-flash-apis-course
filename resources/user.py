4c1d8c93b244e5095ab92db0b5b1cc624cc448e3
willee <ilikechillis@gmail.com>
2020-09-19 00:57:46 +0100

    Added user model and resource.

---
+++ resources/user.py
@@ -0,0 +1,45 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be empty"
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be empty"
+                        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "User already exists."}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message": "User created successfully."}, 201
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User {} not found'.format(user_id)}, 404
+        return user.json()
+
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User {} not found'.format(user_id)}, 404
+        user.delete_from_db()
+        return {'message': 'User {} deleted'.format(user_id)}, 200
