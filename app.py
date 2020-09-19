0b35ed40d090bb56cd6027005b14653d7a4f60d9
willee <ilikechillis@gmail.com>
2020-09-19 02:05:04 +0100

    Added more user functions.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserList
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -30,6 +30,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserList, '/users')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/user.py
+++ resources/user.py
@@ -27,7 +27,15 @@ class UserRegister(Resource):
 
         return {"message": "User created successfully."}, 201
 
+
 class User(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be empty"
+                        )
+
     @classmethod
     def get(cls, user_id):
         user = UserModel.find_by_id(user_id)
@@ -43,3 +51,25 @@ class User(Resource):
             return {'message': 'User {} not found'.format(user_id)}, 404
         user.delete_from_db()
         return {'message': 'User {} deleted'.format(user_id)}, 200
+
+    def put(self, user_id):
+        data = User.parser.parse_args()
+
+        try:
+            user = UserModel.find_by_id(user_id)
+        except:
+            return {"message": "An error occurred whilst searching for the user"}, 500
+
+        if user:
+            user.password = data['password']
+        else:
+            user = UserModel(user_id, data['username'], data['password'])
+
+        user.upsert()
+
+        return user.json()
+
+
+class UserList(Resource):
+    def get(self):
+        return {'users': [user.json() for user in UserModel.query.all()]}
